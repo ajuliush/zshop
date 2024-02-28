1b6136ecccc79e02f8e5b1b3749f31082f3f8817
ajuliush <ajuliush@gmail.com>
2024-02-28 23:55:05 +0600

    initial commit

---
+++ app/Http/Livewire/TrendeProducts.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\likes;
+use App\Models\product;
+use Illuminate\Support\Facades\DB;
+use Livewire\Component;
+
+class TrendeProducts extends Component
+{
+    public function render()
+    {
+
+        //get 6 products that has most likes
+        $trendeProducts = likes::select('product_id', DB::raw('count(product_id) as counts'))
+        ->groupBy('product_id')
+        ->orderByDesc(DB::raw('MAX(created_at)'))
+        ->where('created_at', '>=', now()->subMonth())
+        ->take(6)
+        ->get();
+    
+    return view('livewire.trende-products', [
+        'trendeProducts' => $trendeProducts
+    ]);    
+    }
+}
